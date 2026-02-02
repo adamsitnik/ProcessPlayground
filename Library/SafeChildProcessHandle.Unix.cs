6d3f7023c789d165cbdc257497184f13c466c96e
Adam Sitnik <adam.sitnik@gmail.com>
2026-02-02 19:51:39 +0100

    remove unused using to show we don't use Sockets anymore

--- Library/SafeChildProcessHandle.Unix.cs
+++ Library/SafeChildProcessHandle.Unix.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
-using System.Net.Sockets;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Threading;
