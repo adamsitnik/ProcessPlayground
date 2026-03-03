6a432f08c7ee82ba8132ab6c3aa3082a8dc6183f
Adam Sitnik <adam.sitnik@gmail.com>
2026-03-03 14:43:34 +0100

    add KillTree benchmarks

---
+++ Benchmarks/KillTree.cs
@@ -0,0 +1,54 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Diagnostics;
+using System.TBA;
+
+namespace Benchmarks;
+
+public class KillTree
+{
+    private Process? _process;
+    private SafeChildProcessHandle? _childProcessHandle;
+
+    [IterationSetup(Target = nameof(Process_Kill_EntireProcessTree))]
+    public void Setup_Process()
+    {
+        ProcessStartInfo startInfo = new()
+        {
+            FileName = "powershell",
+            Arguments = "-Command \"Start-Process timeout -ArgumentList '/t','30','/nobreak' -NoNewWindow; Start-Process timeout -ArgumentList '/t','30','/nobreak' -NoNewWindow; Start-Process timeout -ArgumentList '/t','30','/nobreak' -NoNewWindow; timeout /t 30 /nobreak\"",
+            UseShellExecute = false,
+            RedirectStandardInput = true
+        };
+
+        _process = Process.Start(startInfo);
+    }
+
+    [Benchmark(Baseline = true)]
+    public void Process_Kill_EntireProcessTree()
+    {
+        _process!.Kill(entireProcessTree: true);
+        _process.WaitForExit();
+    }
+
+    [IterationSetup(Target = nameof(SafeProcessHandle_KillProcessGroup))]
+    public void Setup_Sfh()
+    {
+        ProcessStartOptions options = new("powershell")
+        {
+            Arguments = { "-Command", "Start-Process timeout -ArgumentList '/t','30','/nobreak' -NoNewWindow; Start-Process timeout -ArgumentList '/t','30','/nobreak' -NoNewWindow; Start-Process timeout -ArgumentList '/t','30','/nobreak' -NoNewWindow; timeout /t 30 /nobreak" },
+            CreateNewProcessGroup = true
+        };
+
+        using SafeFileHandle stdin = Console.OpenStandardInputHandle();
+        _childProcessHandle = SafeChildProcessHandle.Start(options, input: stdin, output: null, error: null);
+    }
+
+    [Benchmark]
+    public void SafeProcessHandle_KillProcessGroup()
+    {
+        _childProcessHandle!.KillProcessGroup();
+        _childProcessHandle.WaitForExit();
+    }
+}
